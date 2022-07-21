f2b39c060aa4b252c136c3d16040e0017d45d67e
harrypotter360 <orian.latroupe@gmail.com>
2022-07-21 13:41:05 +0200

    WAIT ITS TETRIS TITLE SCREEN ? WITH COLORS ?

--- src/gui.c
+++ src/gui.c
@@ -3,7 +3,12 @@
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Color bgcolor = {224, 255, 208, 255};
+void setIcon(){
+	SDL_Surface* iconSurface;
+        iconSurface = SDL_LoadBMP("icon.bmp");
+        SDL_SetWindowIcon(window, iconSurface);
 
+}
 int usrQuit(SDL_Event* event){
         if(SDL_PollEvent(event)){
                 if(event->type == SDL_QUIT){
@@ -18,23 +23,40 @@ int create_window()
 {
     window = SDL_CreateWindow("GBEmulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,640, 576, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    setIcon();
     SDL_SetRenderDrawColor(renderer, 224, 255, 208, 255);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
     return EXIT_SUCCESS;
 }
+void drawPixel(int x,int y){
+	for(int w=0;w<4;w++){
+		for(int q=0;q<4;q++){
+			SDL_RenderDrawPoint(renderer, q+x, w+y);
+                }
+       }
 
+
+}
 void drawBmp(char* binary,int x,int y){
     int z=0;
     for (int i=y; i<y+8*4; i=i+4)
 	for(int j=x;j<x+8*4;j=j+4){
-	        if(binary[z]=='1'){
-			for(int w=0;w<4;w++){
-				for(int q=0;q<4;q++){
-					SDL_RenderDrawPoint(renderer, q+j, w+i);
-				}
-			}
+		switch(binary[z])
+		{
+			case '1':
+				SDL_SetRenderDrawColor(renderer, 136, 192, 112, 255);
+				drawPixel(j,i);
+				break;
+			case '2':
+				SDL_SetRenderDrawColor(renderer, 52, 104, 86, 255);
+				drawPixel(j,i);
+				break;
+			case '3':
+				SDL_SetRenderDrawColor(renderer, 8, 24, 32, 255);
+				drawPixel(j,i);
+				break;
 		}
 		z++;
     	}
--- src/utils.c
+++ src/utils.c
@@ -16,12 +16,25 @@ void getBinary(uint8_t value,char* finalString){
     finalString[8] = 0;
 }
 void getBmp(uint8_t hex[],char* character){
-    char convert[9];
+    char image1[9];
+    char image2[9];
     int k=0;
     for(int i=0;i<16;i=i+2){
-        getBinary(hex[i],convert);
+        getBinary(hex[i],image1);
+	getBinary(hex[i+1],image2);
         for(int j=0;j<8;j++){
-                character[k+j] = convert[j];
+		if(image1[j]=='0' && image2[j]=='0'){
+	                character[k+j] = '0';
+		}
+		else if (image2[j]=='1' && image1[j]=='0'){
+			character[k+j]='1';
+		}
+		else if(image1[j]=='1' && image2[j]=='0'){
+			character[k+j]='2';
+		}
+		else if(image1[j]=='1' && image2[j]=='1'){
+			character[k+j]='3';
+		}
 	}
         k=k+8;
     }
